5449aa4b126a07f741481d889e5102a130add9c8
caiopy <caiop.sci@hotmail.com>
2022-03-22 23:06:16 -0300

    final app

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL: `${process.env.NODE_ENV === 'production'?'https://dtmoney-mu-tawny.vercel.app//api' :'http://localhost:3000/api'}`,  //endereço que se repete em todas as requisições
+    baseURL: `${process.env.NODE_ENV === 'production'?'https://dtmoney-mu-tawny.vercel.app/api' :'http://localhost:3000/api'}`,  //endereço que se repete em todas as requisições
 })
